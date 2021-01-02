b6b619a8ddeb478940ea73203cc4435a770a21de
Brandon Ashby <bash7325@gmail.com>
2021-01-02 11:57:46 -0700

    add q's array/objects

--- script.js
+++ script.js
@@ -1,17 +1,61 @@
-//Start quiz when START clicked
-document
-  .getElementById("startButton")
-  .addEventListener("click", function () {
-    
-  });
+//variables
+var timeID = document.getElementById("timer");
+var start = document.getElementById("startButton");
+var btn1 = document.getElementById("choiceA");
+var btn2 = document.getElementById("choiceB");
+var btn3 = document.getElementById("choiceC");
+var question = document.getElementById("question");
+var answers = document.getElementById("answers");
 
+//array for questions with choices and correct answer
+var questions = [
+
+  {
+
+      question : "?",
+
+      choiceA : "A",
+
+      choiceB : "B",
+
+      choiceC : "C",
+
+      correct : "A"
+
+  },{
+
+      question : "?",
+
+      choiceA : "A",
+
+      choiceB : "B",
+
+      choiceC : "C",
+
+      correct : "B"
+
+  },{
+
+      question : "?",
+
+      choiceA : "A",
+
+      choiceB : "B",
+
+      choiceC : "C",
+
+      correct : "C"
+
+  }
+
+];
 //Countdown timer begins when START clicked
 document.getElementById("startButton").addEventListener("click", function () {
   var timeleft = 60;
 
   var timer = setInterval(function function1() {
     document.getElementById("timer").innerHTML =
-      timeleft + "&nbsp" + "seconds remaining";
+      timeleft + " " + "seconds remaining";
 
     timeleft -= 1;
     if (timeleft <= 0) {
