adfc44d33e0af9913d9bdd0fddd4848c716ca9a9
Brandon Ashby <bash7325@gmail.com>
2021-01-08 23:21:51 -0700

    fixed scores not displaying

--- script.js
+++ script.js
@@ -20,117 +20,111 @@ var leaderBoardScore = document.getElementById("score-div");
 var returnToQuiz = document.getElementById("return-quiz");
 var highScoresLink = document.getElementById("score-leaderboard");
 var timerEl = document.getElementById("time-spent");
-var startTime = 3;
+var startTime = 76;
 
 var highScores = [];
 var scoreNames = [];
 
 // Timer - starts at 75 sec and countdown to 0
 function timerCountdown() {
-    var timerInterval = setInterval(function() {
-        startTime--;
-        timerEl.textContent = startTime + " seconds remaining";
+  var timerInterval = setInterval(function () {
+    startTime--;
+    timerEl.textContent = startTime + " seconds remaining";
 
-        if(startTime === 0 | qIndex === questions.length) {
-            clearInterval(timerInterval);
-            sendMessage();
-            return;
-        }
-    }, 1000);
-};
-//changes timer to read time is up
+    if ((startTime === 0) | (qIndex === questions.length)) {
+      clearInterval(timerInterval);
+      sendMessage();
+      return;
+    }
+  }, 1000);
+}
+//changes timer to read time is up, alerts user
 function sendMessage() {
-    timerEl.textContent = "Time is up!";
-};
+  timerEl.textContent = "Time is up!";
+  alert("Time is Up!");
+}
 //starts quiz
 function launchQuiz() {
-    quizLaunch.classList.add("hide");
-    questionContainer.classList.remove("hide");  
-    showQuestion();    
-} ;
+  quizLaunch.classList.add("hide");
+  questionContainer.classList.remove("hide");
+  showQuestion();
+}
 //cycle through questions, if reaches end display score
 var qIndex = 0;
 
 function showQuestion() {
-    if (qIndex === questions.length | startTime === 0) {
-        endQuizScreen.classList.remove("hide");
-        questionContainer.classList.add("hide");
-        document.getElementById("correct-incorrect").classList.add("hide");
-        var score = startTime;
-        alert("Quiz over! Your score is " + score);
-        highScores.push(score);
-        
-    } else {
-        questionEl.innerText = questions[qIndex].title;
-        btn1.innerHTML = questions[qIndex].choices[0];
-        btn2.innerHTML = questions[qIndex].choices[1];
-        btn3.innerHTML = questions[qIndex].choices[2];
-        btn4.innerHTML = questions[qIndex].choices[3];
-    }
-};
+  if ((qIndex === questions.length) | (startTime === 0)) {
+    endQuizScreen.classList.remove("hide");
+    questionContainer.classList.add("hide");
+    document.getElementById("correct-incorrect").classList.add("hide");
+    var score = startTime;
+    alert("Quiz over! Your score is " + score);
+    highScores.push(score);
+  } else {
+    questionEl.innerText = questions[qIndex].title;
+    btn1.innerHTML = questions[qIndex].choices[0];
+    btn2.innerHTML = questions[qIndex].choices[1];
+    btn3.innerHTML = questions[qIndex].choices[2];
+    btn4.innerHTML = questions[qIndex].choices[3];
+  }
+}
 
 function selectAnswer() {
-
-    if (event.target.textContent === questions[qIndex].answer) {
-        document.getElementById("correct-incorrect").innerHTML = "CORRECT!";
-    }  else {
-        document.getElementById("correct-incorrect").innerHTML = "WRONG!"
-        startTime = startTime - 10;
-    } 
-
-};
-
-buttonStart.addEventListener("click", function() {
-    timerCountdown();
-    launchQuiz();
+  if (event.target.textContent === questions[qIndex].answer) {
+    document.getElementById("correct-incorrect").innerHTML = "CORRECT!";
+  } else {
+    document.getElementById("correct-incorrect").innerHTML = "WRONG!";
+    startTime = startTime - 10;
+  }
+}
+//start quiz
+buttonStart.addEventListener("click", function () {
+  timerCountdown();
+  launchQuiz();
+});
+//go to next question
+buttonChoices.addEventListener("click", function () {
+  selectAnswer();
+  qIndex++;
+  showQuestion();
 });
 
-buttonChoices.addEventListener("click", function() {
-    selectAnswer();
-    qIndex++;
-    showQuestion();
+//high scores, this is not working need to fix
+submitScore.addEventListener("click", function () {
+  alert("Your score has been submitted");
+  var scoreInitials = document.getElementById("enter-initials").value;
 
-});
+  var playerScore = {
+    initials: [scoreInitials],
+    score: [highScores],
+  };
 
-//high scores
-submitScore.addEventListener("click", function() {
-    alert("Your score has been submitted");
-    var scoreInitials = document.getElementById("enter-initials").value;
+  console.log(playerScore);
 
-    var playerScore = {
-        initials: [scoreInitials],
-        score: [highScores]
-    };
-
-    console.log(playerScore);
-
-    function populateStorage () {
-        localStorage.setItem("player score", JSON.stringify(playerScore));
-        localStorage.setItem("initials", JSON.stringify(scoreInitials));
-    };
-    populateStorage(playerScore);
-    
+  function populateStorage() {
+    localStorage.setItem("playerScore", JSON.stringify(playerScore));
+    localStorage.setItem("initials", JSON.stringify(scoreInitials));
+  }
+  populateStorage(playerScore);
 });
 
 //click high scores, hide question container toggle return to quiz
-highScoresLink.addEventListener("click", function() {
+highScoresLink.addEventListener("click", function () {
+  highScoresDisplay.classList.toggle("hide");
+  quizLaunch.classList.add("hide");
+  endQuizScreen.classList.add("hide");
+  highScoresLink.classList.toggle("hide");
+  returnToQuiz.classList.toggle("hide");
 
-    highScoresDisplay.classList.toggle("hide");
-    quizLaunch.classList.add("hide");
-    endQuizScreen.classList.add("hide");
-    highScoresLink.classList.toggle("hide");
-    returnToQuiz.classList.toggle("hide");
+  var scoreList = JSON.parse(localStorage.getItem("playerScore"));
 
-    var scoreList = JSON.parse(localStorage.getItem("playerScore"));
-    
-    leaderBoardInitials.append.textContent = scoreList.initials;
-    leaderBoardScore.append.textContent = scoreList.score;
-
+  document.getElementById("initials-div").textContent = scoreList.initials;
+  document.getElementById("score-div").textContent = scoreList.score;
 });
 //return to quiz
 returnToQuiz.addEventListener("click", function () {
-    highScoresLink.classList.toggle("hide");
-    returnToQuiz.classList.toggle("hide");
-    quizLaunch.classList.toggle("hide");
-    highScoresDisplay.classList.toggle("hide");
+  highScoresLink.classList.toggle("hide");
+  returnToQuiz.classList.toggle("hide");
+  quizLaunch.classList.toggle("hide");
+  highScoresDisplay.classList.toggle("hide");
 });
